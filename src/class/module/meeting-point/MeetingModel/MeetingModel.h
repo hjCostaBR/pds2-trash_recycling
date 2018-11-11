34fa1e9d18e88bfbea95e23608e58e75330d5cc1
hjcostabr <hjcosta@outlook.com.br>
2018-11-11 19:58:39 -0200

    modelos

---
+++ src/class/module/meeting-point/MeetingModel/MeetingModel.h
@@ -0,0 +1,63 @@
+#ifndef _MEETING_POINT_MODEL_H_
+#define _MEETING_POINT_MODEL_H_
+
+#include <string>
+
+using namespace std;
+
+class MeetingModel
+{
+private:
+    int code;
+    string name;
+    string cep;
+    string state;
+    string city;
+    string neighborhood;
+    string street;
+    int number;
+    string complement;
+    
+
+public:
+    MeetingModel(void){};
+    // ~MeetingModel() {};
+
+    // Codigo
+    void setCode(int code);
+    int getCode(void);
+
+    // Nome
+    void setName(string name);
+    string getName(void);
+
+    // Cep
+    void setCep(string cep);
+    string getCep(void);
+
+    // Estado
+    void setState(string state);
+    string getState(void);
+
+    // Cidade
+    void setCity(string city);
+    string getCity(void);
+
+    // Bairro
+    void setNeighborhood(string neighborhood);
+    string getNeighborhood(void);
+
+    // Rua
+    void setStreet(string street);
+    string getStreet(void);
+
+    // Numero
+    int setNumber(int number);
+    string getNumber(void);
+
+    // Complemento
+    void setComplement(string complement);
+    string getComplement(void);
+};
+
+#endif
---
+++ src/enum/UserTypeEnum.cpp
@@ -0,0 +1,12 @@
+/**
+ * ENUM
+ * Define tipos de usuario do sistema
+ * Trash Recycling System
+ * 
+ * @author hjcostabr
+ */
+enum UserTypeEnum {
+    ADMIN = 100,
+    DONATOR = 101,
+    RECEIVER = 102
+};
